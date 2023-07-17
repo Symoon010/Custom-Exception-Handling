301456537d4c141a272a0bb3cd7babca5f764d5f
Symoon Zadid <symoon.zadid@brainstation-23.com>
2023-07-17 22:21:45 +0600

    Initial commit

---
+++ src/test/java/com/Custom/Exception/Handling/CustomExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Custom.Exception.Handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
